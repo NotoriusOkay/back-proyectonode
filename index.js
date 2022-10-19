03d12720162d14d0f52f1e97bf54a825c6dfe499
NotoriusOkay <erickmme2@gmail.com>
2022-10-19 12:32:31 -0500

    Conexion a base de datos

--- index.js
+++ index.js
@@ -13,10 +13,23 @@ app.use(bodyparser.urlencoded({
 app.use(bodyparser.json())
 
 //Conexion a base de datos 
+console.log(process.env)
+const uri = `mongodb+srv://${process.env.USUARIO}:${process.env.PASSWORD}@cluster0.osgbod5.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Conectado a BD')
+}).catch(e => {
+    console.log('error: ', e)
+})
 
 //Importar rutas 
+const authRoutes = require('./routes/auth')
 
 //Ruta del Middleware
+app.use('/api/user', authRoutes)
+
 app.get('/', (req, res) => {
     res.json({
         estado: true,
